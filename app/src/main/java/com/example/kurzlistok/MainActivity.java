53303d753ea371360108beebac2cebac0840ff29
rSedivy-go <r_sedivy@utb.cz>
2020-02-10 23:50:47 +0100

    added basic offline experience

--- app/src/main/java/com/example/kurzlistok/MainActivity.java
+++ app/src/main/java/com/example/kurzlistok/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kurzlistok;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -13,11 +14,15 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -27,6 +32,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,6 +41,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -44,14 +51,20 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
     private HashMap<String, Double> aktualListok = new HashMap<String, Double>();
     private HashMap<String, Double> zobrazListok = new HashMap<String, Double>();
     SimpleDateFormat dateFormat = new SimpleDateFormat(
             "yyyy-MM-dd");
-    String aktualVyber = "null";
+    TextView textViewVysledok;
+    TextView textViewMenaZ;
+    String zvolenaMena = "CZK";
+    String aktualMenaZ = "CZK";
+    String aktualMenaDo = "CZK";
     Date aktualDatum;
     Date zobrazDatum;
     DatePickerDialog datePickerDialog;
@@ -59,6 +72,9 @@ public class MainActivity extends AppCompatActivity  {
     private RequestQueue mQueue;
     DatePickerDialog picker;
     EditText eText;
+    Context context;
+    boolean doCZK = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,9 +93,9 @@ public class MainActivity extends AppCompatActivity  {
         zobrazListok.put("CZK", 1.0);
         setDropdownMenu(zobrazListok);
         mQueue = Volley.newRequestQueue(this);
-        jsonParse("2010-01-12");
 
-        eText=(EditText) findViewById(R.id.editText1);
+        context = getApplicationContext();
+        eText = (EditText) findViewById(R.id.editText1);
         eText.setInputType(InputType.TYPE_NULL);
         eText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,7 +108,14 @@ public class MainActivity extends AppCompatActivity  {
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                String datum = year + "-" + (monthOfYear) + "-" + dayOfMonth;
+                                String datum = year + "-" +
+                                        (monthOfYear + 1 < 10 ?
+                                                String.format(Locale.getDefault(), "%02d", monthOfYear + 1) :
+                                                monthOfYear + 1)
+                                        + "-" +
+                                        (dayOfMonth < 10 ?
+                                                String.format(Locale.getDefault(), "%02d", dayOfMonth) :
+                                                dayOfMonth);
                                 eText.setText(datum);
                                 jsonParse(datum);
                             }
@@ -100,24 +123,87 @@ public class MainActivity extends AppCompatActivity  {
                 picker.show();
             }
         });
+        EditText field1 = (EditText) findViewById(R.id.editTextHodnota);
+        textViewVysledok = (TextView) findViewById(R.id.vysledok);
+        textViewMenaZ = (TextView) findViewById(R.id.menaZ);
+        field1.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                if (s.length() != 0)
+                    try {
+                        calc(Double.parseDouble(s.toString()));
+                    } catch (Exception e) {
+                        textViewVysledok.setText("Vyskytla sa chyba");
+                    }
+
+
+            }
+        });
+        try {
+          aktualListok = loadMap();
+        } catch (Exception e){
+
+        }
+        jsonParse("latest");
+    }
+
+    @Override
+    protected void onStop() {
+        saveMap(aktualListok);
+        super.onStop();
+    }
+
+    private void calc() {
+        String b = ((EditText) findViewById(R.id.editTextHodnota)).getText().toString();
+        calc(Double.parseDouble(b));
+    }
+
+    private void calc(Double s) {
+        try {
+            DecimalFormat df2 = new DecimalFormat("#.##");
+            String hodnota;
+            if (doCZK) {
+                hodnota = String.valueOf(df2.format(s / aktualListok.get(zvolenaMena)));
+                hodnota = hodnota + " " + aktualMenaDo;
+            } else {
+                hodnota = String.valueOf(df2.format(s * aktualListok.get(zvolenaMena)));
+                hodnota = hodnota + " " + aktualMenaDo;
+            }
+            textViewMenaZ.setText(aktualMenaZ);
+            textViewVysledok.setText(hodnota);
+
+        } catch (Exception e) {
+            textViewVysledok.setText("Error");
+        }
 
     }
+
     private void jsonParse(String url) {
         //2010-01-13
         //latest
-           url= "https://api.exchangeratesapi.io/"+url+"?base=CZK";
+        url = "https://api.exchangeratesapi.io/" + url + "?base=CZK";
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-
+                    Toast.makeText(getApplicationContext(), "loaded", Toast.LENGTH_SHORT).show();
                     JSONObject rates = response.getJSONObject("rates");
                     Iterator keysToCopyIterator = rates.keys();
-                    while(keysToCopyIterator.hasNext()) {
+                    while (keysToCopyIterator.hasNext()) {
                         String key = (String) keysToCopyIterator.next();
                         aktualListok.put(key, Double.valueOf(rates.getString(key)));
                     }
-
                     try {
                         aktualDatum = dateFormat.parse(response.getString("date"));
 
@@ -126,8 +212,17 @@ public class MainActivity extends AppCompatActivity  {
                     }
 
                     setDropdownMenu(aktualListok);
+                    zvolenaMena = aktualListok.keySet().toArray()[0].toString();
                     TextView tw = findViewById(R.id.workingDay);
                     tw.setText(dateFormat.format(aktualDatum));
+                    if (doCZK) {
+                        aktualMenaDo = "CZK";
+                        aktualMenaZ = zvolenaMena;
+                    } else {
+                        aktualMenaDo = zvolenaMena;
+                        aktualMenaZ = "CZK";
+                    }
+                    calc();
 
                 } catch (JSONException e) {
                     Toast.makeText(getApplicationContext(), "Error at games feed", Toast.LENGTH_SHORT).show();
@@ -138,14 +233,56 @@ public class MainActivity extends AppCompatActivity  {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getApplicationContext(), "Error at games feed", Toast.LENGTH_SHORT).show();
+                setDropdownMenu(aktualListok);
+                zvolenaMena = aktualListok.keySet().toArray()[0].toString();
+                TextView tw = findViewById(R.id.workingDay);
+//                tw.setText(dateFormat.format(aktualDatum));
+                if (doCZK) {
+                    aktualMenaDo = "CZK";
+                    aktualMenaZ = zvolenaMena;
+                } else {
+                    aktualMenaDo = zvolenaMena;
+                    aktualMenaZ = "CZK";
+                }
+                calc();
                 error.printStackTrace();
             }
         });
         mQueue.add(request);
     }
 
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
 
-    public void setDropdownMenu(HashMap<String, Double> source){
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.zCZK:
+                if (checked) {
+                    aktualMenaZ = "CZK";
+                    textViewMenaZ.setText(aktualMenaZ);
+                    aktualMenaDo = zvolenaMena;
+                    ((RadioButton) findViewById(R.id.doCZK)).setChecked(false);
+                    doCZK = false;
+                    calc();
+                }
+
+                break;
+            case R.id.doCZK:
+                if (checked) {
+                    aktualMenaZ = zvolenaMena;
+                    textViewMenaZ.setText(aktualMenaZ);
+                    aktualMenaDo = "CZK";
+                    ((RadioButton) findViewById(R.id.zCZK)).setChecked(false);
+                    doCZK = true;
+                    calc();
+                }
+
+                break;
+        }
+    }
+
+    public void setDropdownMenu(HashMap<String, Double> source) {
         ArrayList<String> spinnerArray = new ArrayList<String>(source.keySet());
         Spinner dropdown = findViewById(R.id.spinner1);
         ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>
@@ -159,11 +296,19 @@ public class MainActivity extends AppCompatActivity  {
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
                 try {
-                    Toast.makeText(getApplicationContext(), (String) parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
-                    Double suma = aktualListok.get((String) parent.getItemAtPosition(position));
-                    String sb = suma.toString() +" " + (String) parent.getItemAtPosition(position);
-                    ((TextView)findViewById(R.id.hodnota)).setText(sb);
-                }catch (Exception e){
+                    zvolenaMena = (String) parent.getItemAtPosition(position);
+                    Double suma = aktualListok.get(zvolenaMena);
+                    String sb = suma.toString() + " " + zvolenaMena;
+                    ((TextView) findViewById(R.id.hodnota)).setText(sb);
+                    if (doCZK) {
+                        aktualMenaDo = "CZK";
+                        aktualMenaZ = zvolenaMena;
+                    } else {
+                        aktualMenaDo = zvolenaMena;
+                        aktualMenaZ = "CZK";
+                    }
+                    calc();
+                } catch (Exception e) {
 
                 }
 
@@ -197,6 +342,36 @@ public class MainActivity extends AppCompatActivity  {
 
         return super.onOptionsItemSelected(item);
     }
+    private void saveMap(Map<String,Double> inputMap){
+        SharedPreferences pSharedPref = getApplicationContext().getSharedPreferences("MyVariables", Context.MODE_PRIVATE);
+        if (pSharedPref != null){
+            JSONObject jsonObject = new JSONObject(inputMap);
+            String jsonString = jsonObject.toString();
+            SharedPreferences.Editor editor = pSharedPref.edit();
+            editor.remove("My_map").commit();
+            editor.putString("My_map", jsonString);
+            editor.commit();
+        }
+    }
 
+    private HashMap<String,Double> loadMap(){
+        HashMap<String,Double> outputMap = new HashMap<String,Double>();
+        SharedPreferences pSharedPref = getApplicationContext().getSharedPreferences("MyVariables", Context.MODE_PRIVATE);
+        try{
+            if (pSharedPref != null){
+                String jsonString = pSharedPref.getString("My_map", (new JSONObject()).toString());
+                JSONObject jsonObject = new JSONObject(jsonString);
+                Iterator<String> keysItr = jsonObject.keys();
+                while(keysItr.hasNext()) {
+                    String key = keysItr.next();
+                    Double value = (Double) jsonObject.get(key);
+                    outputMap.put(key, value);
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return outputMap;
+    }
 
 }
